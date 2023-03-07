25b9ce7214f014ea916d0f4f1f9108aed83c714c
Gary Solloway <124901657+GarySolloway@users.noreply.github.com>
2023-03-07 16:11:53 +0000

    Exercise 6 completed

--- directory_exercises.rb
+++ directory_exercises.rb
@@ -34,8 +34,8 @@ def input_students
 end
 
 def print_header
-  puts "The students of Villains Academy"
-  puts "-------------"
+  puts "The students of Villains Academy".center(39)
+  puts "--------------------------------".center(39)
 end
 
 def print(students)
@@ -47,7 +47,7 @@ def print(students)
 end
 
 def print_footer(students)
-  puts "Overall, we have #{students.count} great students"
+  puts "Overall, we have #{students.count} great students".center(40)
 end
 
 students = input_students
