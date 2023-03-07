229ece463417ac34f9e2f83a54aabcd6a219ef7b
Gary Solloway <124901657+GarySolloway@users.noreply.github.com>
2023-03-07 16:58:01 +0000

    Exercise 7 completed

--- directory_exercises.rb
+++ directory_exercises.rb
@@ -1,27 +1,37 @@
 def input_students
+  puts "Please enter the names of the students"
   # create an empty array
   students = []
 
   # while true, repeat this code
   while true do
-    puts "Enter your name"
-    # get name
+    puts "Enter the student's name"
+    # get name or set default
     name = gets.chomp
+    name = "Student ##{students.count + 1}" if name.empty?
 
-    puts "Enter your hobbies, separated by commas"
-    # get hobbies
+    puts "Enter the cohort"
+    # get cohort or set default
+    cohort = gets.chomp.to_sym
+    cohort = "Unknown Cohort" if cohort.empty?
+
+    puts "Enter the student's hobbies, separated by commas"
+    # get hobbies or default
     hobbies = gets.chomp
+    hobbies = "n/a" if hobbies.empty?
 
-    puts "Enter your country of birth"
-    # get country
+    puts "Enter the student's country of birth"
+    # get country or set default
     country = gets.chomp
+    country = "n/a" if country.empty?
 
-    puts "Enter your height (ft in)"
-    # get height
+    puts "Enter the student's height (ft in)"
+    # get height or set default
     height = gets.chomp
+    height = "n/a" if height.empty?
 
     # add the student hash to the array
-    students << {name: name, cohort: :november, hobbies: hobbies, country: country, height: height}
+    students << {name: name, cohort: cohort, hobbies: hobbies, country: country, height: height}
     puts "Now we have #{students.count} students"
 
     puts "To continue, press enter or type 'exit' to quit."
